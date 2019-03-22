5e8bb92cba0c6946bf6dd2ce83bc7b2c15a1c921
quentin-moquay <26006295+quentin-moquay@users.noreply.github.com>
2019-03-23 00:18:25 +0100

    New repacking feature

--- manga-grab.groovy
+++ manga-grab.groovy
@@ -1,3 +1,4 @@
+@Grab("commons-io:commons-io:2.6")
 @Grab("commons-cli:commons-cli:1.4")
 @Grab("net.lingala.zip4j:zip4j:1.3.2")
 import net.lingala.zip4j.core.*
@@ -7,6 +8,7 @@ import java.io.File
 import java.net.URL
 import java.net.URLConnection
 import javax.net.ssl.*
+import org.apache.commons.io.FilenameUtils
 
 // Consts
 int ARG_URL = 0
@@ -25,7 +27,9 @@ usage += "\tEnding chapter : Which chapter number to end\n\n"
 usage += "\tRequest Properties : Each Headers you want to send to the downloading HTTP Request\n\n"
 usage += "Options :\n\n"
 usage += "\t-h or --help : show usages\n\n"
+usage += "\t-o or --only-zip : Only zip, no download\n\n"
 usage += "\t-z or --zip : zip each chapter\n\n"
+usage += "\t-r {x} or --repack {x} : Repack each chapter to {x} pages\n\n"
 usage += "Examples :\n\n"
 usage += "\tmanga-grab.groovy 'http://www.funmanga.com/uploads/chapters/15549/:chapter:/p_:page:.jpg' 1 5 0 0\n"
 usage += "\tmanga-grab.groovy 'http://images.mangafreak.net/mangas/prison_school/prison_school_:tome:/prison_school_:tome:_:page:.jpg' 1 1 1 1 'Cookie:__cfduid=d0a467a3dbccecddbb1dd9a95d24332191549719778;cf_clearance=af4a3c4d175e3ed83973a1923bfc70f96b44ca1e-1549808750-3600-150'\n"
@@ -36,8 +40,15 @@ def cli = new CliBuilder(usage: "")
 cli.with {
     h longOpt: 'help', 'Show usage information'
     z longOpt: 'zip', 'zip each chapter'
+    o longOpt: 'only-zip', 'only-zip, no download'
+    r(longOpt: 'repack', args:1, argName:'x', 'Repack each chapter to x pages')
 }
-def options = cli.parse(args)
+def options
+try {
+  options = cli.parse(args)
+} catch (java.lang.IllegalArgumentException e) {
+
+}
 
 // Exclude HTTPS Problems
 def nullTrustManager = [
@@ -104,59 +115,97 @@ if (!options) {
 args = options.arguments()
 println args
 
-if (args.size() < 5 || options.h) {
-  println(usage)
-  return
-}
-def url = args[ARG_URL]
-def chapter = new PadNumber(pad: args[ARG_CHAPTER_PADDING] as int, value: args[ARG_CHAPTER_START] as int)
+if(!options.o) {
+  if (args.size() < 5 || options.h) {
+    println(usage)
+    return
+  }
+  def url = args[ARG_URL]
+  def chapter = new PadNumber(pad: args[ARG_CHAPTER_PADDING] as int, value: args[ARG_CHAPTER_START] as int)
 
-for(int i = (args[ARG_CHAPTER_START] as int); i <= (args[ARG_CHAPTER_END] as int); i++){
-    final File folder = new File("$chapter")
-    folder.mkdir()
-    def page = new PadNumber(pad: args[ARG_PAGE_PADDING] as int, value: 1)
-    boolean firstTime = true
-    while(true){
-        try {
-            def dl = url.replaceAll(":chapter:","$chapter").replaceAll(":page:","$page")
-            println dl
-            urlToFile(dl,"$chapter/${page}.jpg")
-            firstTime = true
-        }catch(FileNotFoundException e){
-           if(firstTime){
-                // sometimes, on some websites, there is ad instead of page
-                firstTime = false
-           } else {
-                println e
-                // twice nothing, there is nothing more on this chapter
-                break
-           }
-        }
-        page++
-    }
-    chapter++
+  for(int i = (args[ARG_CHAPTER_START] as int); i <= (args[ARG_CHAPTER_END] as int); i++){
+      final File folder = new File("$chapter")
+      folder.mkdir()
+      def page = new PadNumber(pad: args[ARG_PAGE_PADDING] as int, value: 1)
+      boolean firstTime = true
+      while(true){
+          try {
+              def dl = url.replaceAll(":chapter:","$chapter").replaceAll(":page:","$page")
+              println dl
+              urlToFile(dl,"$chapter/${page}.jpg")
+              firstTime = true
+          }catch(FileNotFoundException e){
+             if(firstTime){
+                  // sometimes, on some websites, there is ad instead of page
+                  firstTime = false
+             } else {
+                  println e
+                  // twice nothing, there is nothing more on this chapter
+                  break
+             }
+          }
+          page++
+      }
+      chapter++
+  }
 }
 
-if(options.z) {
-  archiveDir = { File dir ->
-    if(!new File("${dir.name}.cbz").exists()) {
-      ZipFile zipFile = new ZipFile("${dir.name}.cbz")
-      ZipParameters parameters = new ZipParameters()
-      parameters.compressionMethod = Zip4jConstants.COMP_DEFLATE
-      parameters.compressionLevel = Zip4jConstants.DEFLATE_LEVEL_NORMAL
-      dir.eachFileRecurse (groovy.io.FileType.FILES) { file ->
+if(options.z || options.o) {
+  if(options.r) {
+    println options.r
+    int chapter = 0
+    int current = 0
+    int limit = options.r as int
+    def parentDir = new File(".")
+    ZipFile zipFile = new ZipFile("${chapter}.cbz")
+    ZipParameters parameters = new ZipParameters()
+    parameters.compressionMethod = Zip4jConstants.COMP_DEFLATE
+    parameters.compressionLevel = Zip4jConstants.DEFLATE_LEVEL_NORMAL
+    def dirs = []
+    parentDir.eachFileRecurse (groovy.io.FileType.DIRECTORIES) { dirs << it }
+    dirs = dirs.sort { a,b -> a.name as int <=> b.name as int }
+    dirs.each { dir ->
+      println("Dir : " + dir.name)
+      def pages = []
+      dir.eachFileRecurse (groovy.io.FileType.FILES) { pages << it }
+      pages = pages.sort { a,b -> FilenameUtils.getBaseName(a.name) as int <=> FilenameUtils.getBaseName(b.name) as int }
+      pages.each { file ->
+          println(file.name)
           if(file.size() == 0) {
             file.delete()
           } else {
+            String newFileName = "p_${current}.${FilenameUtils.getExtension(file.name)}"
+            println(newFileName)
+            parameters.setFileNameInZip(newFileName)
+            parameters.setSourceExternalStream(true)
+            current++
             zipFile.addFile(file, parameters)
+            if(current > limit){
+              chapter++
+              current = 0
+              zipFile = new ZipFile("${chapter}.cbz")
+            }
           }
       }
     }
+  } else {
+
+      def dir = new File(".")
+      dir.eachFileRecurse (groovy.io.FileType.DIRECTORIES) { File dir ->
+        if(!new File("${dir.name}.cbz").exists()) {
+          ZipFile zipFile = new ZipFile("${dir.name}.cbz")
+          ZipParameters parameters = new ZipParameters()
+          parameters.compressionMethod = Zip4jConstants.COMP_DEFLATE
+          parameters.compressionLevel = Zip4jConstants.DEFLATE_LEVEL_NORMAL
+          dir.eachFileRecurse (groovy.io.FileType.FILES) { file ->
+              if(file.size() == 0) {
+                file.delete()
+              } else {
+                zipFile.addFile(file, parameters)
+              }
+          }
+        }
+      }
   }
 
-  def dir = new File(".")
-  dir.eachFileRecurse (groovy.io.FileType.DIRECTORIES) { file ->
-    println file
-    archiveDir(file)
-  }
 }
