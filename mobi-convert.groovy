7357f8bd94240da2489185818f6ebfa1e3699a1a
quentin-moquay <26006295+quentin-moquay@users.noreply.github.com>
2019-03-29 00:39:34 +0100

    Mobi convert (if Calibre is installed and on path)

---
+++ mobi-convert.groovy
@@ -0,0 +1,47 @@
+import groovy.transform.SourceURI
+
+import java.nio.file.Path
+import java.nio.file.Paths
+
+/**
+ * Work only with Calibre installed on your computer and ebook-convert.exe on path
+ */
+class MobiConvert {
+
+    @SourceURI
+    URI sourceUri
+    Path scriptLocation = Paths.get(sourceUri)
+    def wrap = groovy.util.Eval.me(new File("${scriptLocation.parent}${File.separator}cbz-convert.groovy").text)
+
+    def title
+    void createFile(def title) {
+        this.title = title
+        wrap.createFile("temp-"+title)
+    }
+
+    void addFile(File file) {
+        wrap.addFile(file)
+    }
+
+    void addFile(String newFileName, File file) {
+        wrap.addFile(newFileName, file)
+    }
+
+    void close() {
+        wrap.close()
+
+        Process process = "ebook-convert.exe temp-${title}.cbz ${title}.mobi --no-inline-toc --output-profile kindle_pw".execute()
+        def out = new StringBuffer()
+        def err = new StringBuffer()
+        process.consumeProcessOutput( out, err )
+        process.waitFor()
+        if( out.size() > 0 ) println out
+        if( err.size() > 0 ) println err
+
+        new File("temp-${title}.cbz").delete()
+        println("Wrote ${title}.mobi")
+    }
+
+}
+
+return new MobiConvert()
